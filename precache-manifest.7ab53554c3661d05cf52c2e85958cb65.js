d530c4447a2560e760ec222882a48ae7b298a3f4
Vithusanan Mathiaparanam <vithu@vithusanans-mbp.lan>
2020-05-03 13:12:30 -0400

    Updates

---
+++ precache-manifest.7ab53554c3661d05cf52c2e85958cb65.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a5f356562ecbd73e198b6ee7dd01f592",
+    "url": "/app-portfolio/index.html"
+  },
+  {
+    "revision": "e0aa11327fe3befa7b68",
+    "url": "/app-portfolio/static/css/main.e6a45856.chunk.css"
+  },
+  {
+    "revision": "d7a590e54e1fa5936f20",
+    "url": "/app-portfolio/static/js/2.77a0bec3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/app-portfolio/static/js/2.77a0bec3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e0aa11327fe3befa7b68",
+    "url": "/app-portfolio/static/js/main.b3d6e660.chunk.js"
+  },
+  {
+    "revision": "f07f42ce74bceb8fea88",
+    "url": "/app-portfolio/static/js/runtime-main.521cbf0a.js"
+  },
+  {
+    "revision": "aeaa0a3184132b462f2c45507fd3aae2",
+    "url": "/app-portfolio/static/media/mountain_bg.aeaa0a31.jpg"
+  }
+]);
